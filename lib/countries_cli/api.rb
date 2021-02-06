89711a2e2cc5f5f2139e0534714280b97a903705
Reynelle <Rtalford13@gmail.com>
2021-02-06 10:29:19 -0500

    updated .get data method & intro method

--- lib/countries_cli/api.rb
+++ lib/countries_cli/api.rb
@@ -1,17 +1,17 @@
-require 'pry' 
-require 'rest-client'
-
 
  class API
 
   def self.get_data
     response = RestClient.get('https://restcountries.eu/rest/v2/all')
     #create a variable)
-    country_array = JSON.parse(response)
+    country_array = JSON.parse(response)#["name"]
     country_array.each do |country|
-        Country.new(country)
+      Country.new(country)
+    end
+    binding.pry
+  end
+    
 
-  end 
 end
 
 
